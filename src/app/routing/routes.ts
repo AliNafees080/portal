bf1cd227583a9df4acbd562cc81e7b8c2de0bc05
smtx-AliNafees <ali@softmatics.com>
2018-07-21 14:31:55 +0500

    New work started

---
+++ src/app/routing/routes.ts
@@ -0,0 +1,7 @@
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
+
+export const routes = [
+  { path: '',  component: DashboardComponent },
+  { path: 'dashboard',  component: DashboardComponent }
+];
